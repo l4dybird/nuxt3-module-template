eda5260765bbb627f69735f9fb0fa646f670b93e
l4dybird <future.l4dy61rd@gmail.com>
2023-12-11 00:19:54 +0900

    :package: update package

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,18 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit';
+import { name, version } from '../package.json';
 
-export default defineNuxtModule({
+type ModuleOptions = {};
+
+declare module '@nuxt/schema' {
+  interface NuxtConfig {}
+  interface NuxtOptions {}
+}
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name,
+    version,
+  },
+  defaults: undefined,
   setup(_option, _nuxt) {
     const resolver = createResolver(import.meta.url);
     addPlugin(resolver.resolve('./runtime/HelloWorld'));
